392a1237a5941b1702433c72aaa6f5c78ba5ceb6
Tevronis <mishaenz@yandex.ru>
2018-04-17 12:40:32 +0400

    fix

--- digital_signature/main.py
+++ digital_signature/main.py
@@ -1,15 +1,27 @@
+import argparse
 import os
+
+import sys
 
 import digital_signature.rsa as rsa
 
+FILENAME_IN = 'test.jpg'
+
+
+def init_vars():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-fin', nargs='?', default=FILENAME_IN)
+
+    return parser.parse_args(sys.argv[1:])
+
 
 def main():
-    # dir = input()
-    dir = 'test.exe'
+    args = init_vars()
+    dir = args.fin
     with open(dir, 'rb') as f:
         file = f.read()
     r = rsa.RSA(100)
-    print(list(map(int, file)))
+    # print(list(map(int, file)))
     key_o, key_s = r.getKeys()
     cr = r.encrypt(file, key_s)
     with open(dir + '.crypt', 'w') as f:
@@ -20,7 +32,7 @@ def main():
 
     with open('decrypt_' + dir, 'bw') as f:
         to_write = r.decrypt(file, key_o)
-        print(to_write)
+        # print(to_write)
         to_write = bytes(to_write)
         f.write(to_write)
 
